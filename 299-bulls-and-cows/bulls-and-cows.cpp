bcd23591c15b70f2faa806a8894eebbb88af06f2
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-01-03 13:23:55 +0530

    Time: 3 ms (77.14%) | Memory: 6.8 MB (99.15%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    string getHint(string s, string g) {
-        int c=0;
-        map<char,int> mp1,mp2;
-        for(int i=0;i<s.length();i++){
-            if(s[i]==g[i])  c++;
-            else{
-                mp1[s[i]]++;
-                mp2[g[i]]++;
+    string getHint(string secret, string guess) {
+        int bull = 0;
+        int arr1[10] = {};
+        int arr2[10] = {};
+        int n = secret.size(); // = guess.size()
+
+        for (int i = 0; i < n; ++i) {
+            if (secret[i] == guess[i]) {
+                bull += 1;
+            }
+            else {
+                arr1[secret[i] - '0'] += 1;
+                arr2[guess[i] - '0'] += 1;
             }
         }
-        int a=0;
-        for(auto it:mp2){
-            auto temp=mp1.find(it.first);
-            if(temp!=mp1.end()){
-                a+=min(it.second,temp->second);
-                it.second=0;
-            }
+
+        int cow = 0;
+        for (int i = 0; i < 10; ++i) {
+            cow += min(arr1[i], arr2[i]);
         }
-        string fir,sec;
-        fir=to_string(c);
-        sec=to_string(a);
-        string ret=fir;
-        ret+='A';
-        ret+=sec;
-        ret+='B';
+        
+        string ret = "";
+        ret += to_string(bull);
+        ret += "A";
+        ret += to_string(cow);
+        ret += "B";
         return ret;
     }
 };
