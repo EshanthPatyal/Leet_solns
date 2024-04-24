68062f682a95bbc30d646e030b5c66fee1b8716e
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-04-24 20:08:42 +0530

    Time: 2 ms (50.95%) | Memory: 7 MB (97.26%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> v(38,0);
         int a=0,b=1,
         c=1,d=0;
         if(n==0)return 0;
@@ -10,7 +9,6 @@ public:
         }
 
         for(int i=3; i<=n; i++) {
-            v[i] = v[i-1] + v[i-2] + v[i-3];
             d=a+b+c;
             a=b;
             b=c;
