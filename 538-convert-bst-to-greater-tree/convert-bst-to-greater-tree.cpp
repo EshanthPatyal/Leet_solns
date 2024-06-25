122e34f73a81bac34328b21f7458d63f8a98aeeb
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-06-25 09:54:34 +0530

    Time: 109 ms (5.21%) | Memory: 34 MB (5.49%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode*root,vector<int>& in){
+        if(!root)return;
+        inorder(root->left,in);
+        in.push_back(root->val);
+        inorder(root->right,in);
+    }
+    void bfs(vector<int>& temp,vector<int>&in,TreeNode*r){
+        if(!r)return;
+        int i=0;
+        for(;i<in.size();i++){
+            if(r->val==in[i]){
+                break;
+            }
+        }
+        r->val+=temp[i+1];
+        bfs(temp,in,r->left);
+        bfs(temp,in,r->right);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        vector<int> in;
+        inorder(root,in);
+        vector<int> temp;
+        temp=in;
+        temp.push_back(0);
+        for(int i=temp.size()-2;i>=0;i--){
+            temp[i]+=temp[i+1];
+        }
+        bfs(temp,in,root);
+        return root;        
+    }
+};
