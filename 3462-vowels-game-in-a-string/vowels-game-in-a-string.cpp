34ec6fddd71e15dd440f435b041c03e2dfe15280
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-07-23 10:07:06 +0530

    Time: 39 ms (91.77%) | Memory: 17.5 MB (78.36%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -3,9 +3,10 @@ public:
     bool doesAliceWin(string s) {
         int c=0;
         for(auto it:s){
-            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u')c++; 
+            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u')c++;
+            if(c>0)return true; 
         }
-        if(c==0)return false;
-        return true;
+        // if(c==0)return false;
+        return false;
     }
 };
