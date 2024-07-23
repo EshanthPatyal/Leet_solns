dc65c42e12c654f4109e71c9439be4936316a9f5
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-07-23 10:06:39 +0530

    Time: 45 ms (56.76%) | Memory: 17.4 MB (90.19%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int c=0;
+        for(auto it:s){
+            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u')c++; 
+        }
+        if(c==0)return false;
+        return true;
+    }
+};
