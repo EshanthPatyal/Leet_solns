aad442bf002042adf30de7a481de08bdd32f8672
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-07-25 10:28:04 +0530

    Time: 417 ms (32.86%) | Memory: 71.5 MB (60.44%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void merge(int l,int m,int r,vector<int>&a,vector<int>&t){
+        int i=l,j=m+1;
+        int idx=l;
+        while(i<=m && j<=r){
+            if(a[i]<=a[j]){
+                t[idx]=a[i];
+                i++;
+            }
+            else if(a[i]>a[j]){
+                t[idx]=a[j];
+                j++;
+            }
+            idx++;
+        }
+        while(i<=m){
+            t[idx]=a[i];
+            idx++;i++;
+        }
+        while(j<=r){
+            t[idx]=a[j];
+            idx++;j++;        }
+        for(int k=l;k<=r;k++)a[k]=t[k];
+    }
+    void mergesort(vector<int>&v,int beg,int end,vector<int>&temp){
+        if (beg >= end)
+            return;
+            cout<<beg<<" "<<end<<endl;
+        int mid = beg + (end - beg) / 2;
+        mergesort(v, beg, mid, temp);
+        mergesort(v, mid+1, end, temp);
+        merge(beg, mid , end, v, temp);        
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        vector<int> temp(nums.size());
+        mergesort(nums,0,nums.size()-1,temp);
+        return nums;
+    }
+};
