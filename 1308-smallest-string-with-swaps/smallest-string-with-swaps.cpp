bbcfcda6f77d6c3cf5bafac5a5989a02741157c9
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-07-31 22:11:00 +0530

    Time: 669 ms (5.11%) | Memory: 96.1 MB (5.11%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    void dfs(map<int,vector<int>>& adj,multiset<int>&ind,multiset<char>&al,vector<int>&vis,int i,string& s){
-        ind.insert(i);
-        al.insert(s[i]);
+    void dfs(unordered_map<int,vector<int>>& adj,map<int,int>&ind,map<char,int>&al,vector<int>&vis,int i,string& s){
+        ind.insert({i,1});
+        // al.insert({s[i]});
+        al[s[i]]++;
         vis[i]=1;
         for(auto it:adj[i]){
             if(vis[it]==1)continue;
@@ -13,25 +14,27 @@ public:
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
         int n=s.length();
         vector<int> vis(n,0);
-        map<int,vector<int>> adj;
+        unordered_map<int,vector<int>> adj;
         for(auto it:pairs){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
         for(int i=0;i<n;i++){
             if(vis[i]==1)continue;
-            multiset<int>indices;
-            multiset<char>alphas;
+            map<int,int>indices;
+            map<char,int>alphas;
             dfs(adj,indices,alphas,vis,i,s);
             vector<int> temp;
             for(auto it:indices){
-                temp.push_back(it);
+                while(it.second--)temp.push_back(it.first);
             }
                 // for(auto it:alphas)cout<<it<<" "<<i<<" ";
             int c=0;
             for(auto it:alphas){
-                s[temp[c]]=it;
-                c++;
+                while(it.second--){
+                    s[temp[c]]=it.first;
+                    c++;
+                }
             }
         }
         return s;
