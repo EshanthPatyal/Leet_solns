08b5e54601d16bd9fee46a2649b1d0e2a56b9e74
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-08-17 20:04:29 +0530

    Time: 4 ms (75.09%) | Memory: 10.3 MB (80.76%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> ret;
+    map<int,int>mp;
+    void recur(vector<int>&t,int n){
+        if(t.size()==n){
+            ret.push_back(t);
+            return;
+        }
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if(it->second>0){
+                t.push_back(it->first);
+                it->second--;
+                recur(t,n);
+                t.pop_back();
+                it->second++;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> t;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        // for(auto it:mp)cout<<it.first<<" "<<it.second<<endl;
+        recur(t,n);
+        return ret;
+    }
+};
