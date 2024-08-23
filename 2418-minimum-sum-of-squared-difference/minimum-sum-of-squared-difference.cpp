dd8d053b45fee1ba9b6f4a0a573e1d87a593e58a
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-08-23 13:27:07 +0530

    Time: 192 ms (31.92%) | Memory: 122.9 MB (22.55%) - LeetSync

---
+++ 2418-minimum-sum-of-squared-difference/minimum-sum-of-squared-difference.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    long long minSumSquareDiff(vector<int>& nums1, vector<int>& nums2, int k, int k2) {
+        long long out=0;
+        int n=nums1.size();
+        unordered_map<long long , long long>mp;
+        for(int i=0;i<n;i++){
+            mp[abs(nums1[i]-nums2[i])]++;
+            out+=abs(nums1[i]-nums2[i]);
+        }
+        priority_queue<long long>   pq;
+        for(auto it:mp){
+            if(it.first>0){
+                pq.push(it.first);
+            }
+        }
+        k+=k2;
+        if(k>=out)return 0;
+
+        while(k && !pq.empty()){
+            int curr=pq.top();
+            int count=mp[curr];
+            if(count<=k){
+                mp.erase(curr);
+                pq.pop();
+                curr--;
+                k-=count;
+                if(mp.find(curr)==mp.end())pq.push(curr);
+                mp[curr]+=count;
+            }
+            else{
+                curr--;
+                mp[curr]+=k;
+                if(mp.find(curr)==mp.end())pq.push(curr);
+                mp[++curr]=count-k;
+                k=0;
+            }
+        }
+    
+        long long ans = 0;
+        
+        for(auto it:mp){
+            ans+=it.first*1LL*it.first*1LL*it.second;   
+        }
+        return ans;        
+
+    }
+};
