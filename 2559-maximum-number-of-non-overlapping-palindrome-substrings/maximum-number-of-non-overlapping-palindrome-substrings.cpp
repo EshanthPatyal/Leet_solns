7f191c6efc8435449accc2f0c62de234df5dee1a
EshanthPatyal <108993604+EshanthPatyal@users.noreply.github.com>
2024-09-01 01:14:55 +0530

    Time: 4 ms (90.57%) | Memory: 8.2 MB (86.29%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -13,9 +13,17 @@ public:
         curr=s;
         n=s.length();
         vector<int> dp(n+1,0);
+        // vector<vector<int>> pal(n,vector<int>(n,0));
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         pal[i][j]=check(i,j);
+        //         pal[j][i]=check(j,i);
+        //     }
+        // }
         for(int i=0;i<n;i++){
             dp[i]=max(dp[i],i==0?0:dp[i-1]);
-            for(int j=i-k+1;j>=0;j--){
+            int curr=0;
+            for(int j=i-k+1;curr<2 && j>=0; curr++ , j--){
                 dp[i]=max(dp[i],(j==0?0:dp[j-1])+check(j,i));
             }
         }
